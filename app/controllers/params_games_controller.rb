638067444a5d833b5a0792686ae8cb9cdd9b6de3
degel55 <chicubfan@gmail.com>
2016-08-30 18:52:00 -0500

    name done

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -1,2 +1,9 @@
 class ParamsGamesController < ApplicationController
+  def game
+    @name = params[:name].upcase
+  end
+
+  def number_game
+    @number
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/game' => 'params_games#game'
+  get '/number_game' => 'params_games#number_game'
 end
