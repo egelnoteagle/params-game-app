fad17ba76f26debacd41774660757bf9d3c7bd86
degel55 <chicubfan@gmail.com>
2016-08-30 21:14:41 -0500

    further updates

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -4,17 +4,41 @@ class ParamsGamesController < ApplicationController
   end
 
   def a_name
-    @a_name = params[:name]
-    if @a_name.starts_with?("a")
+    @a_name = params[:a_name]
+    if @a_name.start_with?("a")
       @message = "Your name starts with an A."
     end 
   end
 
   def number_game
-    @number = params[:number]
+    @number = params[:number].to_i
+    correct_answer = 55
+    if number == correct_answer
+      @message = "You did it!"
+      elsif number < correct_answer
+        @message = "Guess higher."
+      elsif number > correct_answer
+        @message = "Guess lower."
+      elsif number < 1 || guess > 100  
+        @message = "Please guess between 1-100"   
+    end
   end
 
-  def hilow
-    
+  def url
+    @awkward = params[:wildcard]
+  end
+
+  def game2
+    guess = params[:guess].to_i
+    correct_answer = 55
+    if guess == correct_answer
+      @message = "You did it!"
+      elsif guess < correct_answer
+        @message = "Guess higher."
+      elsif guess > correct_answer
+        @message = "Guess lower."
+      elsif guess < 1 || guess > 100  
+        @message = "Please guess between 1-100"   
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/game' => 'params_games#game'
-  get '/a_name' => 'params_games#a_game'
+  get '/a_name' => 'params_games#a_name'
   get '/number_game' => 'params_games#number_game'
+  get '/url_segment_example/:wildcard' => 'params_games#url'
+  get '/guess_a_number/:guess' => 'params_games#game2'
+  get '/guessing_game_url' => 'params_games#guess_url'
 end
